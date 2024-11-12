1edc4ea1d24c55a2b932740acd0d8467bd8ccb69
CEREZO GUZMAN SOLEDAD <scerezoguz@gmail.com>
2024-11-11 22:54:27 -0400

    Ejercicio 3  en raya

---
+++ Tareas/TareaSegundoParcial/script.js
@@ -0,0 +1,31 @@
+function ejercicio1() {
+    var ajax = new XMLHttpRequest();
+    ajax.open("GET", `tresenraya.html`, true);
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+            document.getElementById("contenido").innerHTML = ajax.responseText;
+            asignarEventosCuadros();
+        }
+    };
+    ajax.send();
+}
+
+function asignarEventosCuadros() {
+    const contenedor = document.getElementById("content-ejercicio1");
+    let turno = "X";
+    const turnoX = document.getElementById("turnoX");
+
+    contenedor.addEventListener("click", (e) => {
+        if (e.target.id == "cuadro" && e.target.innerHTML == "") {
+            e.target.innerHTML = turno;
+            if (turno == "X") {
+                turno = "O";
+            } else {
+                turno = "X";
+            }
+            turnoX.innerHTML = `Turno ${turno}`;
+        }
+    });
+}
+  
+
